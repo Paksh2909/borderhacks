e8990f454d1859e4a313934b18bc03e059dcd3d0
hritvik <hritvik.agarwal@gmail.com>
2020-09-26 00:51:42 +0530

    doc db 80%

--- public/docs.js
+++ public/docs.js
@@ -1,11 +1,41 @@
-const pharmList = document.querySelector('')
+const docList = document.querySelector('#doc-list');
+const form = document.querySelector('#add-doc-form');
 
-function renderPharm(doc){}
+function renderPharm(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let spec = document.createElement('span');
+    let loc = document.createElement('span');
+    let contact = document.createElement('span');
 
-db.collection('pharmacies').get().then((snapshot) => {
+    li.setAttribute('data-id',doc.id);
+    name.textContent = doc.data().name;
+    loc.textContent = doc.data().location;
+    spec.textContent = doc.data().specialization;
+    contact.textContent = doc.data().contact;
+
+    li.appendChild(name);
+    li.appendChild(loc);
+    li.appendChild(spec);
+    li.appendChild(contact);
+
+    docList.appendChild(li);
+}
+
+db.collection('doctors').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
-        console.log(doc.data());
+        renderPharm(doc);
     });
-    
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('doctors').add({
+        name: form.name.value,
+        location: form.location.value,
+        specialization: form.specialization.value,
+        contact: form.contact.value
+
+    })
 
 })
